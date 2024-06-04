9e5838555e0b8e329220aaec0b843c3d8cbe2b40
barearojo <barearojojuan@gmail.com>
2024-06-04 15:04:57 +0200

    principio combinatoria 2

--- scripts/combinatoria.py
+++ scripts/combinatoria.py
@@ -4,7 +4,8 @@ from itertools import product
 categorias = [
     ['Infancia','Niñez', 'Adolescente', 'Adultez','Ancianidad'],  # Edad
     ['Hombre', 'Mujer','No especificado'],# Sexo
-    ['Sí embarazado', 'No embarazado']                        # Embarazo
+    ['Sí embarazado', 'No embarazado'],# Embarazo
+    ['No tiene hijos', 'Tiene un hijo', 'Tiene mas de un hijo']#Hijos
 ]
 
 # Generamos todas las combinaciones posibles
@@ -13,10 +14,13 @@ combinaciones = list(product(*categorias))
 # Filtramos las combinaciones según las condiciones dadas
 combinaciones_filtradas = [
     combinacion for combinacion in combinaciones 
-    if not (combinacion[2] == 'Sí embarazado' and (combinacion[0] != 'Adultez' or combinacion[1] != 'Mujer'))
+    if not (combinacion[2] == 'Sí embarazado' and (combinacion[0] != 'Adultez' or combinacion[1] != 'Mujer')) and
+       not (combinacion[0] in ['Infancia', 'Niñez', 'Adolescente'] and combinacion[3] != 'No tiene hijos')
 ]
 
 # Imprimimos las combinaciones filtradas
 for combinacion in combinaciones_filtradas:
     print(combinacion)
+
+print('Numero total de combianciones = ',len(combinaciones_filtradas))
 
